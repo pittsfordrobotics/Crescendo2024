a4ea393b16c17f71470421623bc9e6d619d801b0
James Mou <95831855+Hammyboi@users.noreply.github.com>
2024-02-21 19:35:18 -0500

    only left motor works

--- src/main/java/frc/robot/Constants/ClimberConstants.java
+++ src/main/java/frc/robot/Constants/ClimberConstants.java
@@ -5,10 +5,10 @@ public class ClimberConstants {
     public static final int CAN_CLIMBER_L = 31;
     public static final int CAN_CLIMBER_R = 32;
 
-    public static final double CAN_CLIMBER_P = .05;
+    public static final double CAN_CLIMBER_P = .5;
     public static final double CAN_CLIMBER_I = 0;
     public static final double CAN_CLIMBER_D = 0;
 
-    public static final double EXTENSION_SETPOINT = 190;
+    public static final double EXTENSION_SETPOINT = 250;
     public static final double RETRACTION_SETPOINT = 1;
 }
