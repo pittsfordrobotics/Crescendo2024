c64954ddb326929dd578697af854cdfde6b865b3
clmazzac <mazzacane.cam@gmail.com>
2024-02-29 23:00:16 -0500

    commit to be reviewed

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -117,7 +117,7 @@ public class RobotContainer {
         // Swerve
         m_driverController.start().onTrue(new DisabledInstantCommand(swerveSubsystem::zeroGyro));
 
-        m_driverController.a().whileTrue(speakerTargetSteeringCommand.alongWith(null))
+        m_driverController.a().whileTrue(speakerTargetSteeringCommand.alongWith(shooter.autoAimSpeaker(null, intake)));
         Command driveCommand = driveModeChooser.getSelected();
         swerveSubsystem.setDefaultCommand(driveCommand);
         driveModeChooser.onChange(command -> {
