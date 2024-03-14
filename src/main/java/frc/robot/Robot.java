4c2eb7c5da2d67f56fe8080ab090f12811c6b94a
EMiller524 <142175077+EMiller524@users.noreply.github.com>
2024-03-14 16:53:59 -0400

    camera in

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -45,8 +45,8 @@ public class Robot extends LoggedRobot {
     // Instantiate our RobotContainer. This will perform all our button bindings,
     // and put our
     // autonomous chooser on the dashboard.
-    // UsbCamera camera = CameraServer.startAutomaticCapture();
-    // camera.setVideoMode(PixelFormat.kYUYV, 640, 480, 10);
+    UsbCamera camera = CameraServer.startAutomaticCapture();
+    camera.setVideoMode(PixelFormat.kYUYV, 640, 480, 10);
     m_robotContainer = new RobotContainer();
   }
 
