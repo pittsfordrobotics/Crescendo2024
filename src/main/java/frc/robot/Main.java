a1b8ca35d14e4187e24d6a616b75820b9b6e7276
amber <39502367+amb2127@users.noreply.github.com>
2024-03-14 19:31:58 -0700

    Update Main.java

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -16,7 +16,7 @@ public final class Main {
 
   /**
    * Main initialization function. Do not perform any initialization here.
-   *
+   * lattim sarpap
    * <p>If you change your main robot class, change the parameter type.
    */
   public static void main(String... args) {
