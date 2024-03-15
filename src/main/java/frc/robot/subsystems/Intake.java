b25eedddd5be92d546139873a5da255adbe2a6c9
clmazzac <mazzacane.cam@gmail.com>
2024-03-15 10:28:39 -0400

    disable camera, init intake 3 times to try and fix flip, improve 3note to midline from podium side

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -76,6 +76,16 @@ public class Intake extends SubsystemBase {
 
     // Intake Motor
     intakeMotor = new CANSparkMax(IntakeConstants.CAN_INTAKE, MotorType.kBrushless);
+    resetintakemotor(); // TODO: Test
+    try {
+      Thread.sleep(200);
+    } catch (InterruptedException e) {
+    }
+    resetintakemotor();
+    try {
+      Thread.sleep(200);
+    } catch (InterruptedException e) {
+    }
     resetintakemotor();
     try {
       Thread.sleep(200);
